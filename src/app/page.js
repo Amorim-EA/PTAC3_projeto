0a769a21197590311c34217ed5d1af18db27197b
Erick Amorim <101286204+Amorim-EA@users.noreply.github.com>
2023-05-22 18:07:38 -0400

    Cadastro (arquivos e estilos 100% rodando), listagem de alunos (estilos e 100% rodando)

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,32 @@
+'use client'
 
-export default function Home() {
-  const req = await fetch("http://localhost:3003/alunos");
+import Image from 'next/image'
+import style from './page.module.css'
+import Link from 'next/link'
+
+export default async function Home() {
+  const req = await fetch("http://localhost:3001/alunos", {
+    cache:"no-cache"
+  });
   const data = await req.json();
   return (
-   <div>
-      <p>{data.map(element => {
-        element.nome
-        element.num_inscricao
-        element.curso
-      })}
-  </div>
+    <div>
+    <h1 className={style.titulo}>Alunos</h1>
+
+    <nav className={style.barra_nav}>
+      <Link className={style.links} href="/">Alunos</Link>
+      <Link className={style.links} href="/cadastro">Cadastrar</Link>
+    </nav>
+
+    <main className={style.container}>
+      {data.map(item => (
+      <div className={style.card} key={item.id}>
+        <p>Nome: {item.nome}</p>
+        <p>Curso: {item.curso}</p>
+        <p>Numero incrição: {item.num_inscricao}</p>
+      </div>
+      ))} 
+    </main>
+    </div>
   )
 }
